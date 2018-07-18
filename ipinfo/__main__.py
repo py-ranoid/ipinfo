375de377edecdd2ef20d2d02df149d3c32ba6c44
Vishal Gupta <vishalg8897@gmail.com>
2018-07-18 23:14:51 +0530

    Done with __main__

--- ipinfo/__main__.py
+++ ipinfo/__main__.py
@@ -3,7 +3,6 @@ import argparse
 from urllib3 import PoolManager,exceptions
 from pkg_resources import resource_filename
 
-
 def get_all():
     http = PoolManager()
     try :
@@ -19,7 +18,7 @@ def get_country_code():
 def get_country_name(code=None):
     code = get_country_code() if code is None else code
     with open(resource_filename(__name__,'country_names.json')) as f:
-        names = json.loads(f)
+        names = json.load(f)
     return names[code]
 
 def get_city():
@@ -42,3 +41,34 @@ def get_locstring():
 
 def get_coords():
     return get_all()["loc"]
+
+options = """
+cn : Get Country Name only
+cc : Get Country Code only
+ct : Get City only
+rg : Get Region only
+wh : Get location as a string
+lc : Get coordinates
+ip : Get IP Address only
+hn : Get Host Name only
+a  : Get all details.""".strip()
+functions = {
+ 'cn' : get_country_name,
+ 'cc' : get_country_code,
+ 'ct' : get_city,
+ 'rg' : get_region,
+ 'wh' : get_locstring,
+ 'lc' : get_coords,
+ 'ip' : get_ip,
+ 'hn' : get_hostname,
+ 'a'  : get_all
+ }
+
+if __name__ == '__main__':
+    desc = "Get information about user's location using IP Address"
+    parser = argparse.ArgumentParser(description=desc,formatter_class = argparse.RawTextHelpFormatter)
+    parser.add_argument('option',help = options,default = 'a',nargs='?')
+    parser.set_defaults(option='a')
+    args = parser.parse_args()
+    func = functions.get(args.option,get_all)
+    print (func())
