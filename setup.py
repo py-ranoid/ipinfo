ab61d3ba9d8870264c3d558fcdd9664101403a66
Vishal Gupta <vishalg8897@gmail.com>
2018-07-18 23:26:38 +0530

    Bug fixes in setup

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='ipinfo',
       version='0.1',
       description='Module to fetch information about user using IP address',
-      url='https://salsa.debian.org/comfortablydumb-guest/Hello-from-the-Debian-side',
+      url='https://salsa.debian.org/comfortablydumb-guest/ipinfo',
       author='Vishal Gupta',
       author_email='vishalg8897@gmail.com',
-      license='GNU',
+      license='GNU General Public License v3.0',
       package_data={'ipinfo': ['country_names.json']},
       include_package_data=True,
       install_requires=[
