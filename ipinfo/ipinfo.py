18b5f433068404b90786c8c06dac4019b566027d
Vishal Gupta <vishalg8897@gmail.com>
2018-07-18 23:45:36 +0530

    Moved functions to another ipinfo.py

---
+++ ipinfo/ipinfo.py
@@ -0,0 +1,64 @@
+import json
+from urllib3 import PoolManager,exceptions
+from pkg_resources import resource_filename
+
+def get_all():
+    http = PoolManager()
+    try :
+        r = http.request('GET', 'http://ipinfo.io')
+    except exceptions.MaxRetryError:
+        return None
+    json_resp = json.loads(r.data.decode('utf-8'))
+    return json_resp
+
+def get_country_code():
+    return get_all()['country']
+
+def get_country_name(code=None):
+    code = get_country_code() if code is None else code
+    with open(resource_filename(__name__,'country_names.json')) as f:
+        names = json.load(f)
+    return names[code]
+
+def get_city():
+    return get_all()['city']
+
+def get_region():
+    return get_all()['region']
+
+def get_ip():
+    return get_all()['ip']
+
+def get_hostname():
+    return get_all()['hostname']
+
+def get_locstring():
+    resp = get_all()
+    cname = get_country_name(resp['country'])
+    locstring = resp['region']+', '+resp['city']+', '+cname
+    return locstring
+
+def get_coords():
+    return get_all()["loc"]
+
+options = """
+cn : Get Country Name only
+cc : Get Country Code only
+ct : Get City only
+rg : Get Region only
+ls : Get location as a string
+lc : Get coordinates
+ip : Get IP Address only
+hn : Get Host Name only
+a  : Get all details.""".strip()
+functions = {
+ 'cn' : get_country_name,
+ 'cc' : get_country_code,
+ 'ct' : get_city,
+ 'rg' : get_region,
+ 'ls' : get_locstring,
+ 'lc' : get_coords,
+ 'ip' : get_ip,
+ 'hn' : get_hostname,
+ 'a'  : get_all
+ }
